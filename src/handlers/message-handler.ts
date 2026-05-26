1df6dbda87cc31eca1fac2833fcb6a0eeb871692
wainerwise <wainer.bonilla@futurewiseinsurance.com>
2026-05-25 23:32:02 -0600

    fix(handler): update message-handler to use corrected ILLMService
    - Remove unused listDirectory import
    - Wire handleCheckGitHistory into AUTOMATIC_TIMESHEET_REPORT case
    - Update formatGitChangesAsTimesheet call to runAutomaticTimeSheetReport
    - Pass stringified values to runManualTimeSheetReport

--- src/handlers/message-handler.ts
+++ src/handlers/message-handler.ts
@@ -1,9 +1,8 @@
 import * as vscode from 'vscode';
 import { WebviewMessage, UrlEntry, StoredItem, IWebviewManager, ILLMService, ILogger } from '../types';
-import { listDirectory } from '../utils/command';
 import { getGitHistoryForUrls } from '../utils/git';
 import { tryCatch } from '../utils/errors';
-import { COMMANDS, CONFIG } from '../../consts';
+import { COMMANDS, CONFIG } from '../consts';
 
 /**
  * Handles messages from the webview and routes them to appropriate handlers
@@ -26,9 +25,9 @@ export class MessageHandler {
       case CONFIG.INFO_ALERT:
         this.handleShowInfo(webMessage);
         break;
-      // case COMMANDS.AUTOMATIC_TIMESHEET_REPORT:
-      //   await this.handleCheckGitHistory(webMessage);
-      //   break;
+      case COMMANDS.AUTOMATIC_TIMESHEET_REPORT:
+        await this.handleCheckGitHistory(webMessage as { command: typeof COMMANDS.AUTOMATIC_TIMESHEET_REPORT; urls: UrlEntry[]; storedItems?: StoredItem[] });
+        break;
       case COMMANDS.MANUAL_TIMESHEET_REPORT:
         await this.llmService.runManualTimeSheetReport(JSON.stringify(webMessage.values));
         break;
@@ -46,37 +45,37 @@ export class MessageHandler {
     }
   }
 
-  // private async handleCheckGitHistory(message: { command: typeof COMMANDS.AUTOMATIC_TIMESHEET_REPORT; urls: UrlEntry[]; storedItems?: StoredItem[] }): Promise<void> {
-  //   const urls = this.extractUrls(message.urls);
-  //   if (!urls) {
-  //     vscode.window.showErrorMessage('No URLs data received');
-  //     return;
-  //   }
+  private async handleCheckGitHistory(message: { command: typeof COMMANDS.AUTOMATIC_TIMESHEET_REPORT; urls: UrlEntry[]; storedItems?: StoredItem[] }): Promise<void> {
+    const urls = this.extractUrls(message.urls);
+    if (!urls) {
+      vscode.window.showErrorMessage('No URLs data received');
+      return;
+    }
 
-  //   const gitChangesResult = await tryCatch(
-  //     () => getGitHistoryForUrls(urls),
-  //     (lines: string[]) => this.logger.log(lines),
-  //     'getting git history'
-  //   );
-  //   if (!gitChangesResult.ok) {
-  //     this.webviewManager.postMessage({
-  //       command: 'gitHistoryResult',
-  //       result: `Error: ${gitChangesResult.error}`
-  //     });
-  //     return;
-  //   }
+    const gitChangesResult = await tryCatch(
+      () => getGitHistoryForUrls(urls),
+      (lines: string[]) => this.logger.log(lines),
+      'getting git history'
+    );
+    if (!gitChangesResult.ok) {
+      this.webviewManager.postMessage({
+        command: 'gitHistoryResult',
+        result: `Error: ${gitChangesResult.error}`
+      });
+      return;
+    }
 
-  //   const storedItems = Array.isArray(message.storedItems) ? message.storedItems : undefined;
-  //   this.logger.log([`Processing ${gitChangesResult.value.length} git change(s)`]);
-  //   if (storedItems && storedItems.length > 0) {
-  //     this.logger.log([`Including ${storedItems.length} stored item(s)`]);
-  //   }
-  //   const timesheet = await this.llmService.formatGitChangesAsTimesheet(gitChangesResult.value, storedItems);
-  //   this.webviewManager.postMessage({
-  //     command: 'gitHistoryResult',
-  //     result: timesheet
-  //   });
-  // }
+    const storedItems = Array.isArray(message.storedItems) ? message.storedItems : undefined;
+    this.logger.log([`Processing ${gitChangesResult.value.length} git change(s)`]);
+    if (storedItems && storedItems.length > 0) {
+      this.logger.log([`Including ${storedItems.length} stored item(s)`]);
+    }
+    const timesheet = await this.llmService.runAutomaticTimeSheetReport(gitChangesResult.value, storedItems);
+    this.webviewManager.postMessage({
+      command: 'gitHistoryResult',
+      result: timesheet
+    });
+  }
 
   private async handleGetModelInfo(): Promise<void> {
     const result = await this.llmService.getSelectedModelInfo();
